8504a1f306b3d8b08a26ca3d3bfc233fdd3d0396
lkingl123 <lokekingloong@gmail.com>
2025-01-05 16:21:41 -0700

    udpated layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,12 +29,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning // Suppresses hydration mismatch warning during development
